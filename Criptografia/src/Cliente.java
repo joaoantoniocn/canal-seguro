1af592acec93fc6c10a7bede26e3805f9eec0aa2
Joao Antonio C. Nunes <joaoantoniocn@gmail.com>
2016-09-24 18:42:56 -0300

    Estabelecendo uma conexao

--- Criptografia/src/Cliente.java
+++ Criptografia/src/Cliente.java
@@ -14,49 +14,61 @@ import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
 public class Cliente {
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		Usuario c1, c2;
-		c1 = new Usuario();
-		c1.setNome("jose");
-		c1.setCpf("123");
-		byte[] encr = null;
-		String s = Usuario.convertToString(c1);
-		System.out.println("Usuário: " + s);
-		byte[] ba = null;
-
-		try {
-			KeyGenerator kg = KeyGenerator.getInstance("AES");
-			kg.init(256);
-			SecretKey sk = kg.generateKey();
-
-			AES abc = new AES();
-			abc.setPadding(new PKCS7Padding());
-			abc.setKey(sk.getEncoded());
-
-			ba = s.getBytes("UTF-8");
-
-			encr = abc.encrypt(ba);
-			System.out.println("User encrypted: " + encr.length + ": " + Base64.encode(encr));
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		Socket client = new Socket("192.168.0.107", 7022);
-
-		ObjectOutputStream output = new ObjectOutputStream(client.getOutputStream());
-		ObjectInputStream input = new ObjectInputStream(client.getInputStream());
-		// output.writeObject(new Package(encr, ba.length));
-		Scanner scanner = new Scanner(System.in);
-		while (true) {
-			String msg = scanner.nextLine();
-			output.writeObject(msg);
-			//System.out.println(encr);
-			//System.out.println(ba.length);
-
-			String resposta = (String) input.readObject();
-			System.out.println("Servidor: " + resposta);
-		}
+//		Usuario c1, c2;
+//		c1 = new Usuario();
+//		c1.setNome("jose");
+//		c1.setCpf("123");
+//		byte[] encr = null;
+//		String s = Usuario.convertToString(c1);
+//		System.out.println("Usuário: " + s);
+//		byte[] ba = null;
+//
+//		try {
+//			KeyGenerator kg = KeyGenerator.getInstance("AES");
+//			kg.init(256);
+//			SecretKey sk = kg.generateKey();
+//
+//			AES abc = new AES();
+//			abc.setPadding(new PKCS7Padding());
+//			abc.setKey(sk.getEncoded());
+//
+//			ba = s.getBytes("UTF-8");
+//
+//			encr = abc.encrypt(ba);
+//			System.out.println("User encrypted: " + encr.length + ": " + Base64.encode(encr));
+//
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//		Socket client = new Socket("192.168.0.107", 7022);
+//
+//		ObjectOutputStream output = new ObjectOutputStream(client.getOutputStream());
+//		ObjectInputStream input = new ObjectInputStream(client.getInputStream());
+//		// output.writeObject(new Package(encr, ba.length));
+//		Scanner scanner = new Scanner(System.in);
+		
+		// --- iniciando a conexao ---
+		Conexao conexao = new Conexao("192.168.0.107", 7022);
+		conexao.init();
+		// cliente iniciando conexao
+		conexao.iniciarConexao();
+		
+		// servidor esperando conexao
+		//conexao.esperarConexao();
+		conexao.fecharConexao();
+		// ---
+		
+//		while (true) {
+//			String msg = scanner.nextLine();
+//			output.writeObject(msg);
+//			//System.out.println(encr);
+//			//System.out.println(ba.length);
+//
+//			String resposta = (String) input.readObject();
+//			System.out.println("Servidor: " + resposta);
+//		}
 		// client.close();
 		//
 		// Usuario testeModificado = (Usuario) input.readObject();
--- Criptografia/src/Conexao.java
+++ Criptografia/src/Conexao.java
@@ -11,17 +11,17 @@ import java.security.PublicKey;
 import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
 public class Conexao {
-	
+
 	private static KeyPairGenerator assimetrica;
 	private static PrivateKey chavePrivada;
 	private static PublicKey chavePublica;
-	
+
 	private Socket client;
 	private PublicKey chavePublicaDestinatario;
 	private ObjectOutputStream output;
 	private ObjectInputStream input;
-	
-	public Conexao(String ip, int porta){
+
+	public Conexao(String ip, int porta) {
 		try {
 			client = new Socket(ip, porta);
 			output = new ObjectOutputStream(client.getOutputStream());
@@ -31,9 +31,8 @@ public class Conexao {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	public static void init(){
+
+	public static void init() {
 		try {
 			assimetrica = KeyPairGenerator.getInstance("AES");
 		} catch (NoSuchAlgorithmException e) {
@@ -43,8 +42,26 @@ public class Conexao {
 		chavePrivada = assimetrica.generateKeyPair().getPrivate();
 		chavePublica = assimetrica.generateKeyPair().getPublic();
 	}
-	
-	private void enviarChavePublica(){
+
+	/**
+	 * Inicia a conexao fazendo a troca de chaves publicas
+	 */
+	public void iniciarConexao() {
+		enviarChavePublica();
+		receberChavePublicaDestinatario();
+
+	}
+
+	/**
+	 * Espera uma conexao, primeiro recebe a chave publica de quem ta tentando se conectar
+	 * e depois envia sua propria chave publica
+	 */
+	public void esperarConexao() {
+		receberChavePublicaDestinatario();
+		enviarChavePublica();
+	}
+
+	private void enviarChavePublica() {
 		try {
 			output.writeObject(chavePublica);
 		} catch (IOException e) {
@@ -52,16 +69,17 @@ public class Conexao {
 			e.printStackTrace();
 		}
 	}
-	
-	private void receberChavePublicaDestinatario(){
+
+	private void receberChavePublicaDestinatario() {
 		try {
 			chavePublicaDestinatario = (PublicKey) input.readObject();
+			System.out.println("Chave publica destinatario: " + chavePublicaDestinatario.toString());
 		} catch (ClassNotFoundException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	private String convertToString(Object obj) {
 		try {
 			String str;
@@ -76,9 +94,9 @@ public class Conexao {
 			e.printStackTrace();
 		}
 		return null;
-	} 
-	
-	public void fecharConexao(){
+	}
+
+	public void fecharConexao() {
 		try {
 			client.close();
 		} catch (IOException e) {
